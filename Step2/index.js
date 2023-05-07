1bacd5cb0453dca328e0f567299ba11eeec7cd7a
mohasan <hasannejadi80@gmail.com>
2023-05-07 15:51:33 +0330

    Add redis cache

--- Step2/index.js
+++ Step2/index.js
@@ -1,73 +1,61 @@
 require("dotenv").config();
-const bodyParser = require('body-parser');
 const express = require("express");
 const redis = require("redis");
 const axios = require("axios");
+const multer = require("multer");
+const os = require("os");
+
 const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+const localIP = "127.0.0.1";
 const port = process.env.PORT || 3000;
+const hostname = os.hostname();
 
-const client = redis.createClient({
-  host: "redis",
-  port: 6379,
-});
-
-client.on("error", (err) => {
+const redisClient = redis.createClient(process.env.REDIS_PORT, localIP);
+redisClient.on("error", (err) => {
   console.log("Redis error:", err);
 });
+redisClient.connect();
 
-const headers = {
-  apikey: process.env.API_KEY,
-  "Content-Type": "application/json",
-};
-
-app.use(bodyParser.json());
-
-app.post("/api/shorten", async (req, res) => {
+app.post("/api/shorten", upload.single("file"), async (req, res) => {
   try {
-    console.log(req.body);
-    const longUrl = req.body.url;
+    const longUrl = req.body?.url;
+    if (!longUrl) {
+      res.status(422).send("Please send a URL.");
+    }
+    const shortUrl = await redisClient.get(longUrl);
+    if (shortUrl) {
+      console.log("Using cached URL:", shortUrl);
+      res.json({
+        longUrl: longUrl,
+        shortUrl: shortUrl,
+        isCached: true,
+        hostname: hostname,
+      });
+    } else {
+      const apiUrl = process.env.API_URL;
+      const config = {
+        method: "post",
+        url: apiUrl,
+        headers: {
+          apikey: process.env.API_KEY,
+        },
+        data: longUrl,
+      };
+      const apiResponse = await axios(config);
+      const { short_url: shortUrl } = apiResponse.data;
 
-    // Check if the long URL is already cached in Redis
-    client.get(longUrl, async (err, shortUrl) => {
-      if (err) throw err;
-
-      if (shortUrl) {
-        console.log("Using cached URL:", shortUrl);
-        res.json({
-          longUrl: longUrl,
-          shortUrl: shortUrl,
-          isCached: true,
-          hostname: new URL(shortUrl).hostname,
-        });
-      } else {
-        const apiUrl = process.env.API_URL;
-        const headers = {
-          apikey: "0b2YvJXUDna0GoZHS7C7ZZzbLkKyHpXf",
-          "Content-Type": "application/json",
-        };
-        const data = JSON.stringify({
-          url: longUrl,
-        });
-        const config = {
-          method: "post",
-          url: apiUrl,
-          headers: headers,
-          data: data,
-        };
-        const apiResponse = await axios(config);
-        console.log(apiResponse);
-        const { short_url: shortUrl } = apiResponse.data;
-
-        client.setex(longUrl, 300, shortUrl);
-
-        res.json({
-          longUrl: longUrl,
-          shortUrl: shortUrl,
-          isCached: false,
-          hostname: new URL(shortUrl).hostname,
-        });
-      }
-    });
+      // cache for 5 minutes
+      redisClient.setEx(longUrl, 300, shortUrl);
+      res.json({
+        longUrl: longUrl,
+        shortUrl: shortUrl,
+        isCached: false,
+        hostname: hostname,
+      });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
